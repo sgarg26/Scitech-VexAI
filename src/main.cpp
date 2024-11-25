c777ffa0796dc86ca9c08c0ffeb31131dbbec3a1
sgarg26 <siddharth261202@gmail.com>
2024-11-25 14:37:48 -0500

    added initial bot configurations

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,87 @@
-#include "lemlib/api.hpp" // IWYU pragma: keep
 #include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "lemlib/chassis/chassis.hpp"
+#include "pros/abstract_motor.hpp"
 #include "pros/misc.hpp"
+#include "pros/motor_group.hpp"
+
+// lemlib docs:
+// https://lemlib.readthedocs.io/en/stable/tutorials/2_configuration.html
+
+/**
+ * Initialize motors
+ * TODO: add correct motor config
+ */
+pros::MotorGroup
+    left_motors({1, 2, 3},
+                pros::MotorGearset::blue); // left motors on ports 1, 2, 3
+pros::MotorGroup
+    right_motors({4, 5, 6},
+                 pros::MotorGearset::blue); // right motors on ports 4, 5, 6
+
+/**
+ * Drivetrain settings
+ * TODO: add correct track width and wheel diameter.
+ */
+lemlib::Drivetrain drivetrain(&left_motors,             // left motor group
+                              &right_motors,            // right motor group
+                              10,                       // 10 inch track width
+                              lemlib::Omniwheel::NEW_4, // using new 4" omnis
+                              360,                      // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
+
+/**
+ * Odom settings
+ */
+pros::Imu imu(10);
+
+/**
+ * PID settings
+ * TODO: need to tweak again, these are just placeholders.
+ */
+// lateral PID controller
+lemlib::ControllerSettings
+    lateral_controller(10,  // proportional gain (kP)
+                       0,   // integral gain (kI)
+                       3,   // derivative gain (kD)
+                       3,   // anti windup
+                       1,   // small error range, in inches
+                       100, // small error range timeout, in milliseconds
+                       3,   // large error range, in inches
+                       500, // large error range timeout, in milliseconds
+                       20   // maximum acceleration (slew)
+    );
+
+// angular PID controller
+lemlib::ControllerSettings
+    angular_controller(2,   // proportional gain (kP)
+                       0,   // integral gain (kI)
+                       10,  // derivative gain (kD)
+                       3,   // anti windup
+                       1,   // small error range, in degrees
+                       100, // small error range timeout, in milliseconds
+                       3,   // large error range, in degrees
+                       500, // large error range timeout, in milliseconds
+                       0    // maximum acceleration (slew)
+    );
+
+// odometry settings
+lemlib::OdomSensors sensors(
+    nullptr, // vertical tracking wheel 1, set to null
+    nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+    nullptr, // horizontal tracking wheel 1
+    nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a
+             // second one
+    &imu     // inertial sensor
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain,         // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors             // odometry sensors
+);
 
 /**
  * A callback function for LLEMU's center button.
@@ -9,13 +90,13 @@
  * "I was pressed!" and nothing.
  */
 void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+  static bool pressed = false;
+  pressed = !pressed;
+  if (pressed) {
+    pros::lcd::set_text(2, "I was pressed!");
+  } else {
+    pros::lcd::clear_line(2);
+  }
 }
 
 /**
@@ -25,10 +106,11 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+  pros::lcd::initialize();
+  chassis.calibrate();
+  pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
+  pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -76,21 +158,17 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+  pros::Controller controller(pros::E_CONTROLLER_MASTER);
+  // loop forever
+  while (true) {
+    // get left y and right x positions
+    int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int leftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
 
+    // move the robot
+    chassis.arcade(leftY, leftX);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+    // delay to save resources
+    pros::delay(25);
+  }
 }
