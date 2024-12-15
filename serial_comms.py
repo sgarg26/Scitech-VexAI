ad86c302d2dabdd7a71aff2eb4c7fc298969ce20
Siddharth Garg <siddharth261202@gmail.com>
2024-12-14 20:43:31 -0500

    added alt paths

--- serial_comms.py
+++ serial_comms.py
@@ -49,11 +49,11 @@ for leg_position in leg_positions:
 input_cost_grid = np.ones((grid_size, grid_size), dtype=float)
 input_cost_grid[occupancy_grid == 1] = np.inf
 
-path = route_through_array(input_cost_grid, (0, 0), (60, 60))
+path = route_through_array(input_cost_grid, (0, 0), (50, 50))
 real_path = np.array(path[0])
 real_path = real_path*resolution
 
-path2 = route_through_array(input_cost_grid, (60, 60), (30, 94))
+path2 = route_through_array(input_cost_grid, (50, 50), (30, 85))
 real_path2 = np.array(path2[0])
 real_path2 = real_path2*resolution
 
